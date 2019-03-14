6e39c2f0996e8b9d623e8e0ee2424701ceb83fbb
Christian Johansson <christian@cvj.se>
2019-03-14 07:15:21 +0100

    My initial commit

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+//! # Milstian Templates
+//!
+//! ![Milstian Logo](https://raw.githubusercontent.com/cjohansson/milstian-rust-internet-framework/master/html/img/logo1-modified.jpg)
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    #[test]
+    fn test_process()
+    {
+        
+    }
+
+    #[test]
+    fn test_set_variable()
+    {
+        
+    }
+
+    #[test]
+    fn test_set_template_from_string()
+    {
+        
+    }
+
+    #[test]
+    fn test_set_template_from_file()
+    {
+        
+    }
+}
