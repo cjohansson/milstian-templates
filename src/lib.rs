b95e8923fff3b9024be6df327831b101242966bb
Christian Johansson <christian@cvj.se>
2019-03-14 07:25:35 +0100

    More work on defining tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,36 +2,29 @@
 //!
 //! ![Milstian Logo](https://raw.githubusercontent.com/cjohansson/milstian-rust-internet-framework/master/html/img/logo1-modified.jpg)
 
-fn main() {
-    println!("Hello, world!");
+struct template {
+    
 }
 
 #[cfg(test)]
-mod tests
-{
+mod tests {
     use super::*;
 
     #[test]
-    fn test_process()
-    {
-        
-    }
+    fn test_process() {}
 
     #[test]
-    fn test_set_variable()
-    {
-        
-    }
+    fn test_parse() {}
 
     #[test]
-    fn test_set_template_from_string()
-    {
-        
-    }
+    fn test_lex() {}
 
     #[test]
-    fn test_set_template_from_file()
-    {
-        
-    }
+    fn test_set_variable() {}
+
+    #[test]
+    fn test_set_template_from_string() {}
+
+    #[test]
+    fn test_set_template_from_file() {}
 }
